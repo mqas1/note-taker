909acfe2e18b619a7e8b79a85ee69cf4dc2e3bfe
mqas1 <114903874+mqas1@users.noreply.github.com>
2023-02-09 02:27:01 +1100

    connects front end and back end of application

---
+++ routes/notes.js
@@ -0,0 +1,62 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// GET Route for a specific note to display on the page
+notes.get('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/tips.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
+
+// POST Route for a new note
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    
+    res.json(`Note added successfully 🗄`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+// DELETE Route for a specific note
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
+
+      writeToFile('./db/db.json', result);
+
+      res.json(`Item ${noteId} has been deleted 🗑`);
+    });
+});
+
+module.exports = notes;
